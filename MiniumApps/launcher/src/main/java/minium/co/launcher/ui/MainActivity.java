09081495a19cd4be704bbb968acbfb011f6df50d
Shahab <tausiq19@gmail.com>
2016-03-17 11:41:24 +0600

    - adds MainActivity in launcher app

---
+++ MiniumApps/launcher/src/main/java/minium/co/launcher/ui/MainActivity.java
@@ -0,0 +1,12 @@
+package minium.co.launcher.ui;
+
+import android.support.v7.app.AppCompatActivity;
+
+import org.androidannotations.annotations.EActivity;
+
+import minium.co.launcher.R;
+
+@EActivity(R.layout.activity_main)
+public class MainActivity extends AppCompatActivity {
+
+}
