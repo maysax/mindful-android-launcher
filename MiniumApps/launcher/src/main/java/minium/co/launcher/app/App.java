6445c78c3b88a2307122a297877cf9f8dc800893
Shahab <tausiq19@gmail.com>
2016-03-17 11:04:44 +0600

    - adds concrete App in launcher module - adds apt plugin in launcher module

---
+++ MiniumApps/launcher/src/main/java/minium/co/launcher/app/App.java
@@ -0,0 +1,15 @@
+package minium.co.launcher.app;
+
+import org.androidannotations.annotations.EApplication;
+
+import minium.co.core.app.CoreApplication;
+
+/**
+ * Concrete implementation of {@link CoreApplication}
+ * This class will implement specific behaviors that differs with core library
+ *
+ * Created by shahab on 3/17/16.
+ */
+@EApplication
+public class App extends CoreApplication {
+}
