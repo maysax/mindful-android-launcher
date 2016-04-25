30418de6e38077bfee8061459ef2c6313efc2b95
Shahab <shahab.uddin@itconquest.com>
2016-04-25 09:35:14 +0600

    successful incremental build#26

--- MiniumApps/contacts/src/main/java/minium/co/contacts/ui/MainActivity.java
+++ MiniumApps/contacts/src/main/java/minium/co/contacts/ui/MainActivity.java
@@ -1,5 +1,12 @@
 package minium.co.contacts.ui;
 
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.PersistableBundle;
+import android.provider.Contacts;
+
+import com.android.contacts.activities.PeopleActivity;
+
 import org.androidannotations.annotations.EActivity;
 
 import minium.co.contacts.R;
@@ -9,4 +16,10 @@ import minium.co.core.ui.CoreActivity;
 @EActivity(R.layout.activity_main)
 public class MainActivity extends CoreActivity {
 
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        startActivity(new Intent(this, PeopleActivity.class));
+    }
 }
