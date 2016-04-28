10d6c6d28f3338de69a0b1129ea5f7d4ce33518e
Shahab <shahab.uddin@itconquest.com>
2016-04-28 11:08:52 +0600

    Search layout introduces

--- MiniumApps/launcher2/src/main/java/minium/co/launcher2/ui/SearchLayout.java
+++ MiniumApps/launcher2/src/main/java/minium/co/launcher2/ui/SearchLayout.java
@@ -1,37 +1,92 @@
 package minium.co.launcher2.ui;
 
 import android.content.Context;
+import android.text.Editable;
+import android.text.TextWatcher;
 import android.util.AttributeSet;
+import android.view.View;
+import android.widget.EditText;
 import android.widget.LinearLayout;
 
+import com.joanzapata.iconify.widget.IconTextView;
+
 import org.androidannotations.annotations.EViewGroup;
+import org.androidannotations.annotations.ViewById;
+
+import minium.co.launcher2.R;
 
 /**
  * Created by Shahab on 4/26/2016.
  */
-@EViewGroup()
+@EViewGroup(R.layout.search_layout)
 public class SearchLayout extends LinearLayout {
+
+    @ViewById
+    protected EditText txtSearchBox;
+
+    @ViewById
+    protected IconTextView btnClear;
 
     public SearchLayout(Context context) {
         super(context);
+        init();
     }
 
     public SearchLayout(Context context, AttributeSet attrs) {
         super(context, attrs);
+        init();
     }
 
     public SearchLayout(Context context, AttributeSet attrs, int defStyleAttr) {
         super(context, attrs, defStyleAttr);
+        init();
     }
 
     public SearchLayout(Context context, AttributeSet attrs, int defStyleAttr, int defStyleRes) {
         super(context, attrs, defStyleAttr, defStyleRes);
+        init();
+    }
+
+    private void init() {
+        setBackgroundResource(R.drawable.edittext_rounded_corners);
+        setOrientation(HORIZONTAL);
     }
 
     @Override
     protected void onFinishInflate() {
         super.onFinishInflate();
+        setupViews();
+    }
 
+    private void setupViews() {
+        btnClear.setVisibility(INVISIBLE);
 
+        txtSearchBox.addTextChangedListener(new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence s, int start, int count, int after) {
+
+            }
+
+            @Override
+            public void onTextChanged(CharSequence s, int start, int before, int count) {
+
+            }
+
+            @Override
+            public void afterTextChanged(Editable s) {
+                if (s.length() != 0) {
+                    btnClear.setVisibility(VISIBLE);
+                } else {
+                    btnClear.setVisibility(INVISIBLE);
+                }
+            }
+        });
+
+        btnClear.setOnClickListener(new OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                txtSearchBox.getText().clear();
+            }
+        });
     }
 }
