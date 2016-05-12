79b3991df9c2ebb3ebdd353178cb11290d97ba96
Shahab <shahab.uddin@itconquest.com>
2016-05-12 10:32:19 +0600

    init flow app

---
+++ MiniumApps/flow/src/main/java/minium/co/flow/ui/MainActivity.java
@@ -0,0 +1,14 @@
+package minium.co.flow.ui;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import org.androidannotations.annotations.EActivity;
+
+import minium.co.core.ui.CoreActivity;
+import minium.co.flow.R;
+
+@EActivity(R.layout.activity_main)
+public class MainActivity extends CoreActivity {
+
+}
