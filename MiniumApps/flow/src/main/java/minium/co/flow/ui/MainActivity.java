ee25f1c68e7cf50cf095cdf6d02fea0b8062a378
Shahab <shahab.uddin@itconquest.com>
2016-05-17 12:12:14 +0600

    recurring check if the notification time is up chime sound

--- MiniumApps/flow/src/main/java/minium/co/flow/ui/MainActivity.java
+++ MiniumApps/flow/src/main/java/minium/co/flow/ui/MainActivity.java
@@ -6,6 +6,11 @@ import android.animation.ObjectAnimator;
 import android.content.Context;
 import android.content.DialogInterface;
 import android.content.Intent;
+import android.media.AudioManager;
+import android.media.MediaPlayer;
+import android.media.Ringtone;
+import android.media.RingtoneManager;
+import android.net.Uri;
 import android.service.notification.NotificationListenerService;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
@@ -14,10 +19,13 @@ import android.view.animation.AccelerateDecelerateInterpolator;
 
 import org.androidannotations.annotations.AfterViews;
 import org.androidannotations.annotations.EActivity;
+import org.androidannotations.annotations.KeyUp;
 import org.androidannotations.annotations.SystemService;
+import org.androidannotations.annotations.Trace;
 import org.androidannotations.annotations.UiThread;
 import org.androidannotations.annotations.ViewById;
 
+import java.io.IOException;
 import java.util.Random;
 
 import minium.co.core.log.Tracer;
@@ -35,21 +43,61 @@ public class MainActivity extends CoreActivity {
     VerticalProgressBar vpBar;
 
     private boolean isAnimationRunning;
+    private boolean isServiceRunning;
     private float progress;
     private final float SPAN = 60 * 1000f;
-    private final float INTERVAL = 5 * 1000f;
+    private final float INTERVAL = 15 * 1000f;
     private final int ANIMATION_DURATION = 500;
 
     @AfterViews
     void afterViews() {
         progress = SPAN;
         setPercentage(1);
-        updateUI();
+        playSound();
     }
 
     @UiThread(delay = 1000L)
     void updateUI() {
+        progress += 1000;
+        Tracer.d("updateUI " + progress);
+        animate();
+        if (progress < SPAN)
+            updateUI();
+        else {
+            playSound();
+            NotificationListener_.intent(this).extra("start", false).start();
+            isServiceRunning = false;
+            finish();
+        }
+    }
 
+    void playSound() {
+        Uri defaultRingtoneUri = RingtoneManager.getDefaultUri(RingtoneManager.TYPE_NOTIFICATION);
+
+        MediaPlayer mediaPlayer = new MediaPlayer();
+
+        try {
+            mediaPlayer.setDataSource(this, defaultRingtoneUri);
+            mediaPlayer.setAudioStreamType(AudioManager.STREAM_NOTIFICATION);
+            mediaPlayer.prepare();
+            mediaPlayer.setOnCompletionListener(new MediaPlayer.OnCompletionListener() {
+
+                @Override
+                public void onCompletion(MediaPlayer mp)
+                {
+                    mp.release();
+                }
+            });
+            mediaPlayer.start();
+        } catch (IllegalArgumentException e) {
+            e.printStackTrace();
+        } catch (SecurityException e) {
+            e.printStackTrace();
+        } catch (IllegalStateException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
 
     @Override
@@ -86,14 +134,10 @@ public class MainActivity extends CoreActivity {
             }
 
             @Override
-            public void onAnimationCancel(Animator animation) {
-
-            }
+            public void onAnimationCancel(Animator animation) {}
 
             @Override
-            public void onAnimationRepeat(Animator animation) {
-
-            }
+            public void onAnimationRepeat(Animator animation) {}
         });
         set.setInterpolator(new AccelerateDecelerateInterpolator());
         set.start();
@@ -103,20 +147,22 @@ public class MainActivity extends CoreActivity {
         vpBar.setSmoothPercent(percentage, ANIMATION_DURATION);
     }
 
-    @Override
-    public boolean onKeyUp(int keyCode, KeyEvent event) {
-        if (keyCode == KeyEvent.KEYCODE_VOLUME_UP) {
-            Tracer.d("onKeyUp: Volume up");
-
-            if (!isAnimationRunning && progress > 0) {
-                if (!ServiceUtils.isMyServiceRunning(this, NotificationListener_.class))
-                    NotificationListener_.intent(this).extra("start", true).start();
-//                notifService.requestInterruptionFilter(NotificationListenerService.INTERRUPTION_FILTER_NONE);
-                progress -= INTERVAL;
-                animate();
+    @KeyUp(KeyEvent.KEYCODE_VOLUME_UP)
+    void onVolumeUpKeyPressed() {
+        Tracer.d("onKeyUp: Volume up");
+        if (!isAnimationRunning && progress > 0) {
+            if (!isServiceRunning) {
+                NotificationListener_.intent(this).extra("start", true).start();
+                updateUI();
+                isServiceRunning = true;
             }
-            return true;
+            progress -= INTERVAL;
+            animate();
         }
-        return super.onKeyUp(keyCode, event);
+    }
+
+    @KeyUp(KeyEvent.KEYCODE_BACK)
+    void onBackKeyPressed() {
+        Tracer.d("onKeyUp: Back");
     }
 }
