914a471ad3947f84b04b4d4d13897ef3fd0f7a65
Shahab <shahab.uddin@itconquest.com>
2016-07-27 11:15:22 +0600

    helper text on notification scheduler

--- MiniumApps/launcher2/src/main/java/minium/co/launcher2/notificationscheduler/NotificationSchedulerFragment.java
+++ MiniumApps/launcher2/src/main/java/minium/co/launcher2/notificationscheduler/NotificationSchedulerFragment.java
@@ -3,11 +3,14 @@ package minium.co.launcher2.notificationscheduler;
 
 import android.support.v4.app.Fragment;
 import android.widget.NumberPicker;
+import android.widget.TextView;
 
 import org.androidannotations.annotations.AfterViews;
 import org.androidannotations.annotations.EFragment;
 import org.androidannotations.annotations.ViewById;
 import org.androidannotations.annotations.sharedpreferences.Pref;
+
+import java.util.Locale;
 
 import minium.co.core.app.DroidPrefs_;
 import minium.co.core.ui.CoreFragment;
@@ -22,8 +25,13 @@ public class NotificationSchedulerFragment extends CoreFragment {
     @ViewById
     NumberPicker valPicker;
 
+    @ViewById
+    TextView txtMsg;
+
     @Pref
     DroidPrefs_ prefs;
+
+    private String[] pickerData = new String[] { "0", "5", "10", "15", "20", "25", "30", "35", "40", "45", "50", "55", "60" };
 
 
     public NotificationSchedulerFragment() {
@@ -33,17 +41,25 @@ public class NotificationSchedulerFragment extends CoreFragment {
     @AfterViews
     void afterViews() {
         valPicker.setMinValue(0);
-        valPicker.setMaxValue(12);
-        valPicker.setDisplayedValues(new String[]{ "0", "5", "10", "15", "20", "25", "30", "35", "40", "45", "50", "55", "60" });
+        valPicker.setMaxValue(pickerData.length - 1);
+        valPicker.setDisplayedValues(pickerData);
         valPicker.setWrapSelectorWheel(true);
         valPicker.setValue(prefs.notificationScheduleIndex().get());
+        updateUI(prefs.notificationScheduleIndex().get());
 
         valPicker.setOnValueChangedListener(new NumberPicker.OnValueChangeListener() {
             @Override
             public void onValueChange(NumberPicker picker, int oldVal, int newVal) {
+                updateUI(newVal);
                 prefs.notificationScheduleIndex().put(newVal);
             }
         });
     }
 
+    private void updateUI(int newVal) {
+        switch (newVal) {
+            case 0: txtMsg.setText("Notification is enabled always"); break;
+            default: txtMsg.setText(String.format(Locale.US, "Notification will be enabled for every %s minutes", pickerData [newVal]));
+        }
+    }
 }
