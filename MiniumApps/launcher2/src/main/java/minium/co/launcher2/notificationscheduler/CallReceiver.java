d4a637be58e925ca0bd544ce94d0c657ac85ee97
Shahab <shahab.uddin@itconquest.com>
2016-07-29 12:40:22 +0600

    reject call experiment works

--- MiniumApps/launcher2/src/main/java/minium/co/launcher2/notificationscheduler/CallReceiver.java
+++ MiniumApps/launcher2/src/main/java/minium/co/launcher2/notificationscheduler/CallReceiver.java
@@ -1,9 +1,14 @@
 package minium.co.launcher2.notificationscheduler;
 
 import android.content.Context;
+import android.telephony.TelephonyManager;
+
+import com.android.internal.telephony.ITelephony;
 
 import org.androidannotations.annotations.EReceiver;
+import org.androidannotations.annotations.SystemService;
 
+import java.lang.reflect.Method;
 import java.util.Date;
 
 import minium.co.core.log.Tracer;
@@ -12,9 +17,13 @@ import minium.co.launcher2.model.MissedCallItem;
 @EReceiver
 public class CallReceiver extends PhonecallReceiver {
 
+    @SystemService
+    TelephonyManager telephonyManager;
+
     @Override
     protected void onIncomingCallStarted(Context ctx, String number, Date start) {
         Tracer.d("onIncomingCallStarted()");
+        //rejectCalls();
     }
 
     @Override
@@ -37,5 +46,20 @@ public class CallReceiver extends PhonecallReceiver {
         Tracer.d("onMissedCall()");
         MissedCallItem callItem = new MissedCallItem(number, start, 0);
         callItem.save();
+    }
+
+    private void rejectCalls() {
+        try {
+            Class c = Class.forName(telephonyManager.getClass().getName());
+            Method m = c.getDeclaredMethod("getITelephony");
+            m.setAccessible(true);
+            ITelephony telephonyService = (ITelephony) m.invoke(telephonyManager);
+
+            telephonyService.silenceRinger();
+            telephonyService.endCall();
+
+        } catch (Exception e) {
+            Tracer.e(e, e.getMessage());
+        }
     }
 }
