d9e96160dc06a95d50c1f8e6a8a9b41c632edbc5
Shahab <shahab.uddin@itconquest.com>
2016-10-11 08:23:54 +0600

    enables supress SMS

--- MiniumApps/launcher2/src/main/java/minium/co/launcher2/notificationscheduler/NotificationSchedulerFragment.java
+++ MiniumApps/launcher2/src/main/java/minium/co/launcher2/notificationscheduler/NotificationSchedulerFragment.java
@@ -3,8 +3,11 @@ package minium.co.launcher2.notificationscheduler;
 
 import android.app.AlarmManager;
 import android.app.PendingIntent;
+import android.content.Context;
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.support.v4.app.Fragment;
+import android.widget.CheckBox;
 import android.widget.CompoundButton;
 import android.widget.NumberPicker;
 import android.widget.TextView;
@@ -24,9 +27,12 @@ import minium.co.core.app.DroidPrefs_;
 import minium.co.core.log.Tracer;
 import minium.co.core.ui.CoreFragment;
 import minium.co.core.util.DateUtils;
+import minium.co.core.util.UIUtils;
 import minium.co.launcher2.R;
 import minium.co.launcher2.events.NotificationSchedulerEvent;
+import minium.co.launcher2.flow.SiempoNotificationService;
 import minium.co.launcher2.flow.SiempoNotificationService_;
+import minium.co.launcher2.utils.ServiceUtils;
 
 /**
  * A simple {@link Fragment} subclass.
@@ -39,6 +45,12 @@ public class NotificationSchedulerFragment extends CoreFragment {
 
     @ViewById
     TextView txtMsg;
+
+    @ViewById
+    CheckBox chkSuppressCalls;
+
+    @ViewById
+    CheckBox chkSuppressSMS;
 
     @Pref
     DroidPrefs_ prefs;
@@ -62,9 +74,30 @@ public class NotificationSchedulerFragment extends CoreFragment {
         valPicker.setDisplayedValues(pickerData);
         valPicker.setWrapSelectorWheel(true);
         valPicker.setValue(prefs.notificationScheduleIndex().get());
+        chkSuppressCalls.setChecked(prefs.notificationSchedulerSupressCalls().get());
+        chkSuppressSMS.setChecked(prefs.notificationSchedulerSupressSMS().get());
         updateUI(prefs.notificationScheduleIndex().get());
 
         alarmIntent = PendingIntent.getBroadcast(getActivity(), 23, new Intent(getActivity(), NotificationScheduleReceiver_.class).putExtra(NotificationScheduleReceiver.KEY_IS_NOTIFICATION_SCHEDULER, true), 0);
+    }
+
+    @Override
+    public void onResume() {
+        super.onResume();
+
+        if (!isEnabled(getActivity())) {
+            UIUtils.confirm(getActivity(), "Ebb flow manager service is not enabled. Please allow Ebb flow manager to access notification service", new DialogInterface.OnClickListener() {
+                @Override
+                public void onClick(DialogInterface dialog, int which) {
+                    startActivity(new Intent("android.settings.ACTION_NOTIFICATION_LISTENER_SETTINGS"));
+                }
+            });
+        }
+    }
+
+    /** @return True if {@link SiempoNotificationService} is enabled. */
+    public static boolean isEnabled(Context mContext) {
+        return ServiceUtils.isNotificationListenerServiceRunning(mContext, SiempoNotificationService_.class);
     }
 
     private void makeEnabled(int newVal) {
