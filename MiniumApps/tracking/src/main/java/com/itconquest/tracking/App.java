dcc98d70b703a71fc0d5bfe574db8124979b536c
Shahab <shahab.uddin@itconquest.com>
2016-11-24 12:54:40 +0600

    adds tracking app

---
+++ MiniumApps/tracking/src/main/java/com/itconquest/tracking/App.java
@@ -0,0 +1,13 @@
+package com.itconquest.tracking;
+
+import org.androidannotations.annotations.EApplication;
+
+import minium.co.core.app.CoreApplication;
+
+/**
+ * Created by Shahab on 11/24/2016.
+ */
+
+@EApplication
+public class App extends CoreApplication {
+}
