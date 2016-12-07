8a68adf264f6b4952aa941ed5baba32ea168e8a1
Shahab <tausiq19@gmail.com>
2016-12-07 14:48:16 +0600

    notification listener update

--- MiniumApps/tracking/src/main/java/com/itconquest/tracking/listener/NotificationListener.java
+++ MiniumApps/tracking/src/main/java/com/itconquest/tracking/listener/NotificationListener.java
@@ -3,6 +3,8 @@ package com.itconquest.tracking.listener;
 import android.service.notification.NotificationListenerService;
 import android.service.notification.StatusBarNotification;
 
+import com.itconquest.tracking.util.TrackingLogger;
+
 import org.androidannotations.annotations.EService;
 
 import minium.co.core.log.Tracer;
@@ -16,12 +18,14 @@ public class NotificationListener extends NotificationListenerService {
     @Override
     public void onNotificationPosted(StatusBarNotification sbn, RankingMap rankingMap) {
         super.onNotificationPosted(sbn, rankingMap);
+        TrackingLogger.log(sbn.toString(), null);
         Tracer.d("onNotificationPosted " + sbn.toString());
     }
 
     @Override
-    public void onNotificationRemoved(StatusBarNotification sbn, RankingMap rankingMap) {
-        super.onNotificationRemoved(sbn, rankingMap);
-        Tracer.d("onNotificationRemoved " + sbn.toString());
+    public void onNotificationPosted(StatusBarNotification sbn) {
+        super.onNotificationPosted(sbn);
+        TrackingLogger.log(sbn.toString(), null);
+        Tracer.d("onNotificationPosted " + sbn.toString());
     }
 }
