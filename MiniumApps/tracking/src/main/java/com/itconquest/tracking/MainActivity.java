645e5dd187292778488f5541e005fa6c26c208af
Shahab <shahab.uddin@itconquest.com>
2016-12-22 13:20:24 +0600

    merges tracking into launcher2

--- MiniumApps/tracking/src/main/java/com/itconquest/tracking/MainActivity.java
+++ MiniumApps/tracking/src/main/java/com/itconquest/tracking/MainActivity.java
@@ -54,7 +54,7 @@ import minium.co.core.ui.CoreActivity;
 import minium.co.core.util.ServiceUtils;
 import minium.co.core.util.UIUtils;
 
-@EActivity(R.layout.activity_main)
+@EActivity(resName = "activity_main")
 public class MainActivity extends CoreActivity {
 
     public final static int REQUEST_CODE = 20;
--- MiniumApps/tracking/src/main/java/com/itconquest/tracking/receivers/ScreenOnOffReceiver.java
+++ MiniumApps/tracking/src/main/java/com/itconquest/tracking/receivers/ScreenOnOffReceiver.java
@@ -4,7 +4,7 @@ import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
 
-import com.itconquest.tracking.services.ScreenOnOffService_;
+
 import com.itconquest.tracking.util.TrackingLogger;
 
 import org.androidannotations.annotations.EReceiver;
