d671bf9a628001bc2876a02606e37b0c2b3b1bc4
Shahab <shahab.uddin@itconquest.com>
2017-02-03 08:59:50 +0600

    adds location permission and marshmallow prompt

--- MiniumApps/launcher2/src/main/java/minium/co/launcher2/MainActivity.java
+++ MiniumApps/launcher2/src/main/java/minium/co/launcher2/MainActivity.java
@@ -117,7 +117,9 @@ public class MainActivity extends CoreActivity implements OnContactSelectedListe
                     .setPermissions(Manifest.permission.READ_CONTACTS,
                             Manifest.permission.READ_CALL_LOG,
                             Manifest.permission.WRITE_EXTERNAL_STORAGE,
-                            Manifest.permission.READ_EXTERNAL_STORAGE)
+                            Manifest.permission.READ_EXTERNAL_STORAGE,
+                            Manifest.permission.ACCESS_FINE_LOCATION,
+                            Manifest.permission.ACCESS_COARSE_LOCATION)
                     .check();
 
             if (!isEnabled(this)) {
