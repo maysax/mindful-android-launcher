fe7a28269ec3da3584891b07c6eee878a0765588
Shahab <shahab.uddin@itconquest.com>
2017-02-23 14:12:37 +0600

    Updates pause UI

--- MiniumApps/launcher3/src/main/java/co/minium/launcher3/ui/PauseActivity.java
+++ MiniumApps/launcher3/src/main/java/co/minium/launcher3/ui/PauseActivity.java
@@ -1,16 +1,23 @@
 package co.minium.launcher3.ui;
 
+import android.os.Vibrator;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.Toolbar;
+
+import com.jesusm.holocircleseekbar.lib.HoloCircleSeekBar;
 
 import org.androidannotations.annotations.AfterViews;
 import org.androidannotations.annotations.Click;
 import org.androidannotations.annotations.EActivity;
 import org.androidannotations.annotations.Fullscreen;
+import org.androidannotations.annotations.SystemService;
 import org.androidannotations.annotations.ViewById;
+import org.androidannotations.annotations.sharedpreferences.Pref;
 
 import co.minium.launcher3.R;
+import co.minium.launcher3.app.Launcher3Prefs_;
+import minium.co.core.app.DroidPrefs_;
 import minium.co.core.ui.CoreActivity;
 import minium.co.core.util.UIUtils;
 
@@ -21,9 +28,19 @@ public class PauseActivity extends CoreActivity {
     @ViewById
     Toolbar toolbar;
 
+    @ViewById
+    HoloCircleSeekBar seekbar;
+
+    @Pref
+    Launcher3Prefs_ launcherPrefs;
+
+    @SystemService
+    Vibrator vibrator;
+
     @AfterViews
     void afterViews() {
         setSupportActionBar(toolbar);
+        seekbar.setOnSeekBarChangeListener(seekbarListener);
     }
 
     @Click
@@ -35,5 +52,23 @@ public class PauseActivity extends CoreActivity {
     void settingsActionBar() {
         UIUtils.alert(this, getString(R.string.msg_not_yet_implemented));
     }
+
+    private HoloCircleSeekBar.OnCircleSeekBarChangeListener seekbarListener = new HoloCircleSeekBar.OnCircleSeekBarChangeListener() {
+
+        @Override
+        public void onProgressChanged(HoloCircleSeekBar seekBar, int progress, boolean fromUser) {
+
+        }
+
+        @Override
+        public void onStartTrackingTouch(HoloCircleSeekBar seekBar) {
+
+        }
+
+        @Override
+        public void onStopTrackingTouch(HoloCircleSeekBar seekBar) {
+
+        }
+    };
 
 }
