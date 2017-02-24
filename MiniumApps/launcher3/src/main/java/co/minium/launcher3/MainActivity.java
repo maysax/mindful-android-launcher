31369a217d1ec4d10cdaf416811aeebae064fb84
asad <itc@itconquest.com>
2017-02-24 17:17:16 +0600

    Added overlaw for default status bar and tray

--- MiniumApps/launcher3/src/main/java/co/minium/launcher3/MainActivity.java
+++ MiniumApps/launcher3/src/main/java/co/minium/launcher3/MainActivity.java
@@ -1,9 +1,23 @@
 package co.minium.launcher3;
 
 import android.Manifest;
+import android.app.Activity;
+import android.content.Context;
+import android.content.Intent;
+import android.graphics.PixelFormat;
+import android.net.Uri;
+import android.os.Build;
+import android.provider.Settings;
+import android.support.annotation.Nullable;
 import android.support.v4.view.ViewPager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.Gravity;
+import android.view.MotionEvent;
+import android.view.ViewGroup;
+import android.view.WindowManager;
+import android.widget.Toast;
 
 import com.gun0912.tedpermission.PermissionListener;
 import com.gun0912.tedpermission.TedPermission;
@@ -30,10 +44,14 @@ import static minium.co.core.log.LogConfig.TRACE_TAG;
 @EActivity(R.layout.activity_main)
 public class MainActivity extends CoreActivity {
 
+    public static final int OVERLAY_PERMISSION_REQ_CODE = 4545;
+    protected static customViewGroup blockingView = null;
+
     @ViewById
     ViewPager pager;
 
     MainSlidePagerAdapter sliderAdapter;
+
 
     @Trace(tag = TRACE_TAG)
     @AfterViews
@@ -48,13 +66,15 @@ public class MainActivity extends CoreActivity {
                         Manifest.permission.ACCESS_FINE_LOCATION,
                         Manifest.permission.ACCESS_COARSE_LOCATION)
                 .check();
-
         sliderAdapter = new MainSlidePagerAdapter(getFragmentManager());
         pager.setAdapter(sliderAdapter);
+
     }
+
 
     private void loadViews() {
         loadTopBar();
+
         //loadMainView();
     }
 
@@ -78,4 +98,114 @@ public class MainActivity extends CoreActivity {
             UIUtils.toast(MainActivity.this, "Permission denied");
         }
     };
+
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        modifyStatusBar();
+    }
+    @Trace(tag="statusbar")
+    public void modifyStatusBar() {
+        if (android.os.Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+
+            if (!Settings.canDrawOverlays(this)) {
+                Toast.makeText(this, "Allow Siempo to Use the notification tray!", Toast.LENGTH_SHORT).show();
+                Intent intent = new Intent(Settings.ACTION_MANAGE_OVERLAY_PERMISSION, Uri.parse("package:" + getPackageName()));
+                startActivityForResult(intent, OVERLAY_PERMISSION_REQ_CODE);
+            } else {
+                preventStatusBarExpansion(this);
+            }
+        }
+        else {
+            preventStatusBarExpansion(this);
+        }
+    }
+
+    @Trace(tag="statusbar")
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+
+        super.onActivityResult(requestCode, resultCode, data);
+
+        if (requestCode == OVERLAY_PERMISSION_REQ_CODE) {
+            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+                if (!Settings.canDrawOverlays(this)) {
+                    Toast.makeText(this, "User can access system settings without this permission!", Toast.LENGTH_SHORT).show();
+                }
+                else
+                {
+                    preventStatusBarExpansion(this);
+                }
+            }
+        }
+    }
+
+    // adding for preventing notification bar
+    @Trace(tag = "statusbar")
+    public  void preventStatusBarExpansion(Context context) {
+        WindowManager manager = ((WindowManager) context.getApplicationContext()
+                .getSystemService(Context.WINDOW_SERVICE));
+
+        Activity activity = (Activity)context;
+        WindowManager.LayoutParams localLayoutParams = new WindowManager.LayoutParams();
+        localLayoutParams.type = WindowManager.LayoutParams.TYPE_SYSTEM_ERROR;
+        localLayoutParams.gravity = Gravity.TOP;
+        localLayoutParams.flags = WindowManager.LayoutParams.FLAG_NOT_FOCUSABLE|
+
+                // this is to enable the notification to recieve touch events
+                WindowManager.LayoutParams.FLAG_NOT_TOUCH_MODAL |
+
+                // Draws over status bar
+                WindowManager.LayoutParams.FLAG_LAYOUT_IN_SCREEN;
+
+        localLayoutParams.width = WindowManager.LayoutParams.MATCH_PARENT;
+        int resId = activity.getResources().getIdentifier("status_bar_height", "dimen", "android");
+        int result = 0;
+        if (resId > 0) {
+            result = activity.getResources().getDimensionPixelSize(resId);
+        }
+
+        localLayoutParams.height = result;
+
+        localLayoutParams.format = PixelFormat.TRANSPARENT;
+
+        blockingView = new customViewGroup(context);
+
+        manager.addView(blockingView, localLayoutParams);
+    }
+
+    public static class customViewGroup extends ViewGroup {
+
+        public customViewGroup(Context context) {
+            super(context);
+        }
+
+        @Override
+        protected void onLayout(boolean changed, int l, int t, int r, int b) {
+        }
+
+        @Override
+        public boolean onInterceptTouchEvent(MotionEvent ev) {
+            Log.v("customViewGroup", "**********Intercepted");
+
+
+
+            return true;
+        }
+    }
+
+
+    @Override
+    protected void onDestroy() {
+
+        super.onDestroy();
+
+        if (blockingView!=null) {
+            WindowManager manager = ((WindowManager) getApplicationContext().getSystemService(Context.WINDOW_SERVICE));
+            manager.removeView(blockingView);
+        }
+    }
+
+
 }
