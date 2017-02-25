99e0bbc2a9be259503b54a4ab7931f6c2feeba9b
Tarun <tarun.pstu@gmail.com>
2017-02-25 12:10:51 +0600

    Following tasks are done 1. Design pixel perfection 2. listner

--- MiniumApps/launcher3/src/main/java/co/minium/launcher3/ui/RecyclerViewAdapter.java
+++ MiniumApps/launcher3/src/main/java/co/minium/launcher3/ui/RecyclerViewAdapter.java
@@ -2,7 +2,6 @@ package co.minium.launcher3.ui;
 
 import android.content.Context;
 import android.support.v7.widget.RecyclerView;
-import android.support.v7.widget.Toolbar;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
