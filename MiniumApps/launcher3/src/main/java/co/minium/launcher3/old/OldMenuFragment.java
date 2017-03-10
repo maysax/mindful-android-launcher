92b0dff82ba8604beac623b9a78d060b345bdf29
Shahab <shahab.uddin@itconquest.com>
2017-03-10 16:05:04 +0600

    removes unused theme

--- MiniumApps/launcher3/src/main/java/co/minium/launcher3/old/OldMenuFragment.java
+++ MiniumApps/launcher3/src/main/java/co/minium/launcher3/old/OldMenuFragment.java
@@ -2,6 +2,7 @@ package co.minium.launcher3.old;
 
 
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Build;
 import android.os.Bundle;
 import android.os.Handler;
@@ -26,6 +27,7 @@ import co.minium.launcher3.call.CallLogActivity_;
 import co.minium.launcher3.contact.PhoneNumbersAdapter;
 import co.minium.launcher3.helper.ActivityHelper;
 import co.minium.launcher3.map.SiempoMapActivity_;
+import co.minium.launcher3.mm.TimePickerActivity;
 import co.minium.launcher3.model.MainListItem;
 import co.minium.launcher3.pause.PauseActivity_;
 import co.minium.launcher3.ui.TempoActivity_;
@@ -104,6 +106,7 @@ public class OldMenuFragment extends CoreFragment {
                 SiempoMapActivity_.intent(getActivity()).start(); break;
             case 12: new ActivityHelper(getActivity()).handleDefaultLauncher((CoreActivity) getActivity()); break;
             case 13:
+                startActivity(new Intent(getActivity(), TimePickerActivity.class));
                 // mindful morning
                 break;
             case 14: break;
