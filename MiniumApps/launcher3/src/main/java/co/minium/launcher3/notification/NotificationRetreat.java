7d8634a33110972e4abc1197c352e509018234f4
Tarun <tarun.pstu@gmail.com>
2017-04-21 19:00:13 +0600

    added try and catch

--- MiniumApps/launcher3/src/main/java/co/minium/launcher3/notification/NotificationRetreat.java
+++ MiniumApps/launcher3/src/main/java/co/minium/launcher3/notification/NotificationRetreat.java
@@ -69,31 +69,36 @@ public class NotificationRetreat {
     }
 
     private void notificationBuilder(StatusBarNotificationStorage storage) {
-        NotificationCompat.Builder mBuilder =
-                new NotificationCompat.Builder(context)
-                        .setSmallIcon(R.drawable.ic_siempo_notification)
-                        .setContentTitle(getAppName(storage.getPackageName()))
-                        .setContentText(storage.getContent())
-                .setAutoCancel(true);
-        // Creates an explicit intent for an Activity in your app
-        Intent resultIntent = context.getPackageManager().getLaunchIntentForPackage(storage.getPackageName());
 
-        // The stack builder object will contain an artificial back stack for the
-        // started Activity.
-        // This ensures that navigating backward from the Activity leads out of
-        // your application to the Home screen.
-        TaskStackBuilder stackBuilder = TaskStackBuilder.create(context);
-        // Adds the back stack for the Intent (but not the Intent itself)
-        //stackBuilder.addParentStack(ResultActivity.class);
-        // Adds the Intent that starts the Activity to the top of the stack
-        stackBuilder.addNextIntent(resultIntent);
-        PendingIntent resultPendingIntent =
-                stackBuilder.getPendingIntent(
-                        0,
-                        PendingIntent.FLAG_UPDATE_CURRENT
-                );
-        mBuilder.setContentIntent(resultPendingIntent);
-        // mId allows you to update the notification later on.
-        notificationManager.notify(storage.getId().intValue(), mBuilder.build());
+        try {
+            NotificationCompat.Builder mBuilder =
+                    new NotificationCompat.Builder(context)
+                            .setSmallIcon(R.drawable.ic_siempo_notification)
+                            .setContentTitle(getAppName(storage.getPackageName()))
+                            .setContentText(storage.getContent())
+                    .setAutoCancel(true);
+            // Creates an explicit intent for an Activity in your app
+            Intent resultIntent = context.getPackageManager().getLaunchIntentForPackage(storage.getPackageName());
+
+            // The stack builder object will contain an artificial back stack for the
+            // started Activity.
+            // This ensures that navigating backward from the Activity leads out of
+            // your application to the Home screen.
+            TaskStackBuilder stackBuilder = TaskStackBuilder.create(context);
+            // Adds the back stack for the Intent (but not the Intent itself)
+            //stackBuilder.addParentStack(ResultActivity.class);
+            // Adds the Intent that starts the Activity to the top of the stack
+            stackBuilder.addNextIntent(resultIntent);
+            PendingIntent resultPendingIntent =
+                    stackBuilder.getPendingIntent(
+                            0,
+                            PendingIntent.FLAG_UPDATE_CURRENT
+                    );
+            mBuilder.setContentIntent(resultPendingIntent);
+            // mId allows you to update the notification later on.
+            notificationManager.notify(storage.getId().intValue(), mBuilder.build());
+        } catch (Exception e) {
+            Tracer.e(e,e.getMessage());
+        }
     }
 }
