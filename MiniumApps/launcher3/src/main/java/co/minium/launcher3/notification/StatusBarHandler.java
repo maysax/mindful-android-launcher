bcdcd0fdf76651aff183f549c77b2ec9ddd2c43b
Tarun <tarun.pstu@gmail.com>
2017-04-21 19:48:00 +0600

    added try and catch

--- MiniumApps/launcher3/src/main/java/co/minium/launcher3/notification/StatusBarHandler.java
+++ MiniumApps/launcher3/src/main/java/co/minium/launcher3/notification/StatusBarHandler.java
@@ -4,6 +4,7 @@ import android.app.Activity;
 import android.app.FragmentTransaction;
 import android.content.Context;
 import android.content.Intent;
+import android.content.res.Resources;
 import android.graphics.PixelFormat;
 import android.os.Build;
 import android.provider.Settings;
@@ -17,6 +18,7 @@ import java.util.ArrayList;
 import java.util.List;
 
 import co.minium.launcher3.R;
+import minium.co.core.log.Tracer;
 import minium.co.core.ui.CoreActivity;
 
 /**
@@ -63,38 +65,43 @@ public class StatusBarHandler {
     // preventStatusBarExpansion
 
     private void preventStatusBarExpansion() {
-        System.out.println(TAG + " preventStatusBarExpansion");
-        WindowManager manager = ((WindowManager) mContext.getApplicationContext()
-                .getSystemService(Context.WINDOW_SERVICE));
 
-        Activity activity = (Activity)mContext;
-        WindowManager.LayoutParams localLayoutParams = new WindowManager.LayoutParams();
-        localLayoutParams.type = WindowManager.LayoutParams.TYPE_SYSTEM_ERROR;
-        localLayoutParams.gravity = Gravity.TOP;
-        localLayoutParams.flags = WindowManager.LayoutParams.FLAG_NOT_FOCUSABLE|
+        try {
+            System.out.println(TAG + " preventStatusBarExpansion");
+            WindowManager manager = ((WindowManager) mContext.getApplicationContext()
+                    .getSystemService(Context.WINDOW_SERVICE));
 
-                // this is to enable the notification to recieve touch events
-                WindowManager.LayoutParams.FLAG_NOT_TOUCH_MODAL |
+            Activity activity = (Activity)mContext;
+            WindowManager.LayoutParams localLayoutParams = new WindowManager.LayoutParams();
+            localLayoutParams.type = WindowManager.LayoutParams.TYPE_SYSTEM_ERROR;
+            localLayoutParams.gravity = Gravity.TOP;
+            localLayoutParams.flags = WindowManager.LayoutParams.FLAG_NOT_FOCUSABLE|
 
-                // Draws over status bar
-                WindowManager.LayoutParams.FLAG_LAYOUT_IN_SCREEN;
+                    // this is to enable the notification to recieve touch events
+                    WindowManager.LayoutParams.FLAG_NOT_TOUCH_MODAL |
 
-        localLayoutParams.width = WindowManager.LayoutParams.MATCH_PARENT;
-        int resId = activity.getResources().getIdentifier("status_bar_height", "dimen", "android");
-        int result = 0;
-        if (resId > 0) {
-            result = activity.getResources().getDimensionPixelSize(resId);
+                    // Draws over status bar
+                    WindowManager.LayoutParams.FLAG_LAYOUT_IN_SCREEN;
+
+            localLayoutParams.width = WindowManager.LayoutParams.MATCH_PARENT;
+            int resId = activity.getResources().getIdentifier("status_bar_height", "dimen", "android");
+            int result = 0;
+            if (resId > 0) {
+                result = activity.getResources().getDimensionPixelSize(resId);
+            }
+
+            status_bar_height = result;
+
+            localLayoutParams.height = result;
+
+            localLayoutParams.format = PixelFormat.TRANSPARENT;
+
+            manager.addView(blockingView, localLayoutParams);
+            blockingViewCollection.add(blockingView);
+            isActive = true;
+        } catch (Exception e) {
+            Tracer.e(e,e.getMessage());
         }
-
-        status_bar_height = result;
-
-        localLayoutParams.height = result;
-
-        localLayoutParams.format = PixelFormat.TRANSPARENT;
-
-        manager.addView(blockingView, localLayoutParams);
-        blockingViewCollection.add(blockingView);
-        isActive = true;
     }
 
     private class customViewGroup extends ViewGroup {
