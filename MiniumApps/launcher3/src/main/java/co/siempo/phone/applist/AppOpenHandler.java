17b131befdb2be4b6548d0541e209e69041b6da1
Shahab <shahab.uddin@itconquest.com>
2017-05-18 10:32:23 +0600

    removed duplicate code

--- MiniumApps/launcher3/src/main/java/co/siempo/phone/applist/AppOpenHandler.java
+++ MiniumApps/launcher3/src/main/java/co/siempo/phone/applist/AppOpenHandler.java
@@ -5,10 +5,12 @@ import android.content.Context;
 import java.util.List;
 
 import co.siempo.phone.app.Constants;
+import co.siempo.phone.db.DBClient;
 import co.siempo.phone.db.DBUtility;
 import co.siempo.phone.db.TableNotificationSms;
 import co.siempo.phone.db.TableNotificationSmsDao;
 import co.siempo.phone.event.TopBarUpdateEvent;
+import co.siempo.phone.notification.NotificationUtility;
 import de.greenrobot.event.EventBus;
 
 /**
@@ -19,19 +21,11 @@ public class AppOpenHandler {
 
     public void handle(Context context, AppOpenEvent event) {
         if (AppUtil.isDefaultSmsApp(context, event.getPackageName())) {
-            List<TableNotificationSms> notificationSmsesList = DBUtility.getNotificationDao().queryBuilder()
-                    .where(TableNotificationSmsDao.Properties.Notification_type.eq(0))
-                    .list();
-            DBUtility.getNotificationDao().deleteInTx(notificationSmsesList);
-
+            new DBClient().deleteMsgByType(NotificationUtility.NOTIFICATION_TYPE_SMS);
             EventBus.getDefault().post(new TopBarUpdateEvent());
 
         } else if (event.getPackageName().equals(Constants.CALL_APP_PACKAGE)) {
-            List<TableNotificationSms> notificationSmsesList = DBUtility.getNotificationDao().queryBuilder()
-                    .where(TableNotificationSmsDao.Properties.Notification_type.eq(1))
-                    .list();
-            DBUtility.getNotificationDao().deleteInTx(notificationSmsesList);
-
+            new DBClient().deleteMsgByType(NotificationUtility.NOTIFICATION_TYPE_CALL);
             EventBus.getDefault().post(new TopBarUpdateEvent());
 
         }
