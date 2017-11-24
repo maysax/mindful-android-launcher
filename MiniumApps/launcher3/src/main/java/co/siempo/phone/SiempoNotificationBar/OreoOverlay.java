a1dd6c5fc74bb3b2bbe0022345efa1e5e0aed527
rajeshj <rajesh.jadi@volansystech.com>
2017-11-24 19:07:00 +0530

    SSA-728 Fabric crash : Alpha release 10811- java.lang.RuntimeException: SiempoNotificationBar.OverlayView.gettingNameAndImageFromPhoneNumberCaused by java.lang.SecurityException - check the permission before the bind the data to overlay view and create constant for permission.

--- MiniumApps/launcher3/src/main/java/co/siempo/phone/SiempoNotificationBar/OreoOverlay.java
+++ MiniumApps/launcher3/src/main/java/co/siempo/phone/SiempoNotificationBar/OreoOverlay.java
@@ -49,6 +49,7 @@ import android.widget.RelativeLayout;
 import android.widget.SeekBar;
 import android.widget.TextClock;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import com.bumptech.glide.Glide;
 import com.james.status.data.IconStyleData;
@@ -1300,6 +1301,12 @@ class OreoOverlay extends FrameLayout implements View.OnClickListener {
 
     private void setUpNotifications(List<TableNotificationSms> items) {
         notificationList.clear();
+
+        if (ContextCompat.checkSelfPermission(context, Manifest.permission.READ_CONTACTS) != PackageManager.PERMISSION_GRANTED
+                && ContextCompat.checkSelfPermission(context, Manifest.permission.WRITE_CONTACTS) != PackageManager.PERMISSION_GRANTED) {
+            Toast.makeText(context, "Enable permission from Settings to access Contact details.", Toast.LENGTH_SHORT).show();
+        }
+
         for (int i = 0; i < items.size(); i++) {
             @SuppressLint("SimpleDateFormat") DateFormat sdf = new SimpleDateFormat("hh:mm a");
             String time = sdf.format(items.get(i).get_date());
@@ -1341,35 +1348,38 @@ class OreoOverlay extends FrameLayout implements View.OnClickListener {
     }
 
     private NotificationContactModel gettingNameAndImageFromPhoneNumber(String number) {
+        if (ContextCompat.checkSelfPermission(context, Manifest.permission.READ_CONTACTS) == PackageManager.PERMISSION_GRANTED
+                && ContextCompat.checkSelfPermission(context, Manifest.permission.WRITE_CONTACTS) == PackageManager.PERMISSION_GRANTED) {
+            Uri uri = Uri.withAppendedPath(ContactsContract.PhoneLookup.CONTENT_FILTER_URI, Uri.encode(number));
+            Cursor cursor = context.getContentResolver().query(uri, new String[]{ContactsContract.PhoneLookup.DISPLAY_NAME,
+                    ContactsContract.CommonDataKinds.Phone.PHOTO_URI}, null, null, null);
 
-        Uri uri = Uri.withAppendedPath(ContactsContract.PhoneLookup.CONTENT_FILTER_URI, Uri.encode(number));
-        Cursor cursor = context.getContentResolver().query(uri, new String[]{ContactsContract.PhoneLookup.DISPLAY_NAME,
-                ContactsContract.CommonDataKinds.Phone.PHOTO_URI}, null, null, null);
+            String contactName, imageUrl = "";
+            try {
+                if (cursor != null && cursor.moveToFirst()) {
+                    contactName = cursor.getString(cursor.getColumnIndex(ContactsContract.PhoneLookup.DISPLAY_NAME));
+                    imageUrl = cursor
+                            .getString(cursor
+                                    .getColumnIndex(ContactsContract.CommonDataKinds.Phone.PHOTO_URI));
+                    cursor.close();
 
-        String contactName, imageUrl = "";
-        try {
-            if (cursor != null && cursor.moveToFirst()) {
-                contactName = cursor.getString(cursor.getColumnIndex(ContactsContract.PhoneLookup.DISPLAY_NAME));
-                imageUrl = cursor
-                        .getString(cursor
-                                .getColumnIndex(ContactsContract.CommonDataKinds.Phone.PHOTO_URI));
-                cursor.close();
-
-            } else {
-                contactName = number;
+                } else {
+                    contactName = number;
+                }
+            } catch (Exception e) {
+                contactName = "";
+                imageUrl = "";
+                e.printStackTrace();
             }
-        } catch (Exception e) {
-            contactName = "";
-            imageUrl = "";
-            e.printStackTrace();
+
+
+            NotificationContactModel notificationContactModel = new NotificationContactModel();
+            notificationContactModel.setName(contactName);
+            notificationContactModel.setImage(imageUrl);
+
+            return notificationContactModel;
         }
-
-
-        NotificationContactModel notificationContactModel = new NotificationContactModel();
-        notificationContactModel.setName(contactName);
-        notificationContactModel.setImage(imageUrl);
-
-        return notificationContactModel;
+        return null;
     }
 
     final GestureDetector gesture = new GestureDetector(context,
--- MiniumApps/launcher3/src/main/java/co/siempo/phone/SiempoNotificationBar/OverlayView.java
+++ MiniumApps/launcher3/src/main/java/co/siempo/phone/SiempoNotificationBar/OverlayView.java
@@ -61,6 +61,7 @@ import android.widget.RelativeLayout;
 import android.widget.SeekBar;
 import android.widget.TextClock;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import com.blankj.utilcode.util.LogUtils;
 import com.bumptech.glide.Glide;
@@ -1314,6 +1315,11 @@ class OverlayView extends FrameLayout implements View.OnClickListener {
 
     private void setUpNotifications(List<TableNotificationSms> items) {
         notificationList.clear();
+        if (ContextCompat.checkSelfPermission(context, Manifest.permission.READ_CONTACTS) != PackageManager.PERMISSION_GRANTED
+                && ContextCompat.checkSelfPermission(context, Manifest.permission.WRITE_CONTACTS) != PackageManager.PERMISSION_GRANTED) {
+            Toast.makeText(context, "Enable permission from Settings to access Contact details.", Toast.LENGTH_SHORT).show();
+        }
+
         for (int i = 0; i < items.size(); i++) {
             @SuppressLint("SimpleDateFormat") DateFormat sdf = new SimpleDateFormat("hh:mm a");
             String time = sdf.format(items.get(i).get_date());
@@ -1356,35 +1362,37 @@ class OverlayView extends FrameLayout implements View.OnClickListener {
     }
 
     private NotificationContactModel gettingNameAndImageFromPhoneNumber(String number) {
+        if (ContextCompat.checkSelfPermission(context, Manifest.permission.READ_CONTACTS) == PackageManager.PERMISSION_GRANTED
+                && ContextCompat.checkSelfPermission(context, Manifest.permission.WRITE_CONTACTS) == PackageManager.PERMISSION_GRANTED) {
+            Uri uri = Uri.withAppendedPath(ContactsContract.PhoneLookup.CONTENT_FILTER_URI, Uri.encode(number));
+            Cursor cursor = context.getContentResolver().query(uri, new String[]{ContactsContract.PhoneLookup.DISPLAY_NAME,
+                    ContactsContract.CommonDataKinds.Phone.PHOTO_URI}, null, null, null);
 
-        Uri uri = Uri.withAppendedPath(ContactsContract.PhoneLookup.CONTENT_FILTER_URI, Uri.encode(number));
-        Cursor cursor = context.getContentResolver().query(uri, new String[]{ContactsContract.PhoneLookup.DISPLAY_NAME,
-                ContactsContract.CommonDataKinds.Phone.PHOTO_URI}, null, null, null);
+            String contactName, imageUrl = "";
+            try {
+                if (cursor != null && cursor.moveToFirst()) {
+                    contactName = cursor.getString(cursor.getColumnIndex(ContactsContract.PhoneLookup.DISPLAY_NAME));
+                    imageUrl = cursor
+                            .getString(cursor
+                                    .getColumnIndex(ContactsContract.CommonDataKinds.Phone.PHOTO_URI));
+                    cursor.close();
 
-        String contactName, imageUrl = "";
-        try {
-            if (cursor != null && cursor.moveToFirst()) {
-                contactName = cursor.getString(cursor.getColumnIndex(ContactsContract.PhoneLookup.DISPLAY_NAME));
-                imageUrl = cursor
-                        .getString(cursor
-                                .getColumnIndex(ContactsContract.CommonDataKinds.Phone.PHOTO_URI));
-                cursor.close();
-
-            } else {
-                contactName = number;
+                } else {
+                    contactName = number;
+                }
+            } catch (Exception e) {
+                contactName = "";
+                imageUrl = "";
+                e.printStackTrace();
             }
-        } catch (Exception e) {
-            contactName = "";
-            imageUrl = "";
-            e.printStackTrace();
+
+
+            NotificationContactModel notificationContactModel = new NotificationContactModel();
+            notificationContactModel.setName(contactName);
+            notificationContactModel.setImage(imageUrl);
+            return notificationContactModel;
         }
-
-
-        NotificationContactModel notificationContactModel = new NotificationContactModel();
-        notificationContactModel.setName(contactName);
-        notificationContactModel.setImage(imageUrl);
-
-        return notificationContactModel;
+        return null;
     }
 
     final GestureDetector gesture = new GestureDetector(context,
