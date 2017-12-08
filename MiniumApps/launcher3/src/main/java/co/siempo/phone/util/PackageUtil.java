c35234728c9e6337cf41eb574a886861711f7330
hardikk <hardik.kamothi@volansystech.com>
2017-12-08 17:33:00 +0530

    SSA-853 Notification Manager (Include all apps ) & View Suppress Notification section should be in Alpha Setting         - Put overlay stuff of Action_Screen_on & Off to CoreActivity         - Remove Accessibility Service

--- MiniumApps/launcher3/src/main/java/co/siempo/phone/util/PackageUtil.java
+++ MiniumApps/launcher3/src/main/java/co/siempo/phone/util/PackageUtil.java
@@ -7,10 +7,8 @@ import android.content.pm.ResolveInfo;
 import android.net.Uri;
 import android.os.Build;
 import android.provider.Settings;
-import android.text.TextUtils;
 import android.widget.Toast;
 import co.siempo.phone.R;
-import co.siempo.phone.service.SiempoAccessibilityService;
 import co.siempo.phone.service.SiempoDndService;
 
 /**
@@ -60,11 +58,7 @@ public class PackageUtil {
     }
 
     public static void checkPermission(Context context) {
-        if (!isAccessibilitySettingsOn(context)) {
-            Toast.makeText(context, R.string.msg_accessibility2, Toast.LENGTH_SHORT).show();
-            Intent intent1 = new Intent(android.provider.Settings.ACTION_ACCESSIBILITY_SETTINGS);
-            context.startActivity(intent1);
-        }
+
         if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
             // Show alert dialog to the user saying a separate permission is needed
             // Launch the settings activity if the user prefers
@@ -78,33 +72,4 @@ public class PackageUtil {
 
     }
 
-    private static boolean isAccessibilitySettingsOn(Context mContext) {
-        int accessibilityEnabled = 0;
-        final String service = mContext.getPackageName() + "/" + SiempoAccessibilityService.class.getCanonicalName();
-        try {
-            accessibilityEnabled = Settings.Secure.getInt(
-                    mContext.getApplicationContext().getContentResolver(),
-                    android.provider.Settings.Secure.ACCESSIBILITY_ENABLED);
-        } catch (Settings.SettingNotFoundException e) {
-            e.printStackTrace();
-        }
-        TextUtils.SimpleStringSplitter mStringColonSplitter = new TextUtils.SimpleStringSplitter(':');
-
-        if (accessibilityEnabled == 1) {
-            String settingValue = Settings.Secure.getString(
-                    mContext.getApplicationContext().getContentResolver(),
-                    Settings.Secure.ENABLED_ACCESSIBILITY_SERVICES);
-            if (settingValue != null) {
-                mStringColonSplitter.setString(settingValue);
-                while (mStringColonSplitter.hasNext()) {
-                    String accessibilityService = mStringColonSplitter.next();
-                    if (accessibilityService.equalsIgnoreCase(service)) {
-                        return true;
-                    }
-                }
-            }
-        }
-
-        return false;
-    }
 }
