d84deeb4f9b33fb5c0516265efce929a22de2e2e
Hardik Kamothi <hardik.kamothi@volansystech.com>
2017-12-14 13:44:38 +0530

    SSA-368 Tap and Hold on an app shows option to uninstall and/or app info for the selected app.         - OnLongClickListener implemented and will show bottom sheet dialog with tow options: 1. Uninstall 2. App Info         Uninstall will show confirmation alert to user whether the user surely wants to uninstall the long clicked app or not         while App info will redirect to the selected application information screen

--- MiniumApps/launcher3/src/main/java/co/siempo/phone/applist/InstalledAppListAdapter.java
+++ MiniumApps/launcher3/src/main/java/co/siempo/phone/applist/InstalledAppListAdapter.java
@@ -1,9 +1,16 @@
 package co.siempo.phone.applist;
 
 import android.app.Activity;
+import android.app.Dialog;
+import android.content.ActivityNotFoundException;
+import android.content.Intent;
 import android.content.pm.ApplicationInfo;
 import android.content.pm.PackageManager;
+import android.net.Uri;
+import android.os.Build;
+import android.provider.Settings;
 import android.support.v7.widget.RecyclerView;
+import android.view.Gravity;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -22,30 +29,12 @@ import minium.co.core.log.Tracer;
 
 
 class InstalledAppListAdapter extends RecyclerView.Adapter<InstalledAppListAdapter.ViewHolder> {
+    public static final String TAG = "InstalledAppListAdapter";
     private final Activity context;
     private PackageManager packageManager;
     private List<ApplicationInfo> arrayList;
     private LayoutInflater mInflater;
     private boolean isGrid;
-
-    public class ViewHolder extends RecyclerView.ViewHolder {
-        // each data item is just a string in this case
-        private TextView txt_app_name;
-        private ImageView imv_appicon;
-        public View layout;
-        private View divider;
-        private LinearLayout linearLayout;
-
-        public ViewHolder(View v) {
-            super(v);
-            layout = v;
-            divider = v.findViewById(R.id.divider);
-            linearLayout = v.findViewById(R.id.linearList);
-            txt_app_name = v.findViewById(R.id.txt_app_name);
-            imv_appicon = v.findViewById(R.id.imv_appicon);
-        }
-    }
-
 
     // Provide a suitable constructor (depends on the kind of dataset)
     InstalledAppListAdapter(Activity context, List<ApplicationInfo> arrayList, boolean isGrid) {
@@ -92,6 +81,8 @@ class InstalledAppListAdapter extends RecyclerView.Adapter<InstalledAppListAdapt
         holder.linearLayout.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
+
+
                 try {
                     Tracer.i("Opening package: " + applicationInfo.packageName);
                     new ActivityHelper(context).openAppWithPackageName(applicationInfo.packageName);
@@ -102,12 +93,98 @@ class InstalledAppListAdapter extends RecyclerView.Adapter<InstalledAppListAdapt
                 }
             }
         });
+
+        holder.linearLayout.setOnLongClickListener(new View.OnLongClickListener() {
+            @Override
+            public boolean onLongClick(View v) {
+
+
+                View view = context.getLayoutInflater().inflate(R.layout
+                        .installed_app_bottom_sheet, null);
+                TextView txtDetail = view.findViewById(R.id.txt_detail);
+                final TextView txtUninstall = view.findViewById(R.id.txt_uninstall);
+
+                final Dialog mBottomSheetDialog = new Dialog(context, R.style
+                        .MaterialDialogSheet);
+                mBottomSheetDialog.setContentView(view);
+                mBottomSheetDialog.setCancelable(true);
+                mBottomSheetDialog.getWindow().setLayout(LinearLayout.LayoutParams.MATCH_PARENT, LinearLayout.LayoutParams.WRAP_CONTENT);
+                mBottomSheetDialog.getWindow().setGravity(Gravity.BOTTOM);
+                mBottomSheetDialog.show();
+
+
+                txtDetail.setOnClickListener(new View.OnClickListener() {
+
+                    @Override
+                    public void onClick(View v) {
+                        try {
+                            //Open the specific App Info page:
+                            Intent intent = new Intent(android.provider.Settings.ACTION_APPLICATION_DETAILS_SETTINGS);
+                            intent.setData(Uri.parse("package:" + applicationInfo.packageName));
+                            context.startActivity(intent);
+
+                        } catch ( ActivityNotFoundException e ) {
+                            //e.printStackTrace();
+
+                            //Open the generic Apps page:
+                            Intent intent = new Intent(android.provider.Settings.ACTION_MANAGE_APPLICATIONS_SETTINGS);
+                            context.startActivity(intent);
+
+                        }
+                        mBottomSheetDialog.dismiss();
+                    }
+                });
+
+
+                txtUninstall.setOnClickListener(new View.OnClickListener() {
+
+                    @Override
+                    public void onClick(View v) {
+                        try {
+                            int UNINSTALL_REQUEST_CODE = 2;
+
+                            Intent intent = new Intent(Intent.ACTION_UNINSTALL_PACKAGE);
+                            intent.setData(Uri.parse("package:" + applicationInfo.packageName));
+                            intent.putExtra(Intent.EXTRA_RETURN_RESULT, true);
+                            context.startActivityForResult(intent,
+                                    UNINSTALL_REQUEST_CODE);
+                        } catch (Exception e) {
+                            Tracer.e(e, e.getMessage());
+                        }
+
+                        mBottomSheetDialog.dismiss();
+
+                    }
+                });
+                return false;
+            }
+        });
+
+
     }
 
     // Return the size of your dataset (invoked by the layout manager)
     @Override
     public int getItemCount() {
         return arrayList.size();
+    }
+
+    public class ViewHolder extends RecyclerView.ViewHolder {
+        public View layout;
+        // each data item is just a string in this case
+        private TextView txt_app_name;
+        private ImageView imv_appicon;
+        private View divider;
+        private LinearLayout linearLayout;
+
+        public ViewHolder(View v) {
+            super(v);
+            layout = v;
+            divider = v.findViewById(R.id.divider);
+            linearLayout = v.findViewById(R.id.linearList);
+            txt_app_name = v.findViewById(R.id.txt_app_name);
+            imv_appicon = v.findViewById(R.id.imv_appicon);
+        }
     }
 
 }
