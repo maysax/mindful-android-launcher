72a1aab488ecde23a2517e36c0878d5298b8b67f
rajeshj <rajesh.jadi@volansystech.com>
2018-02-02 14:30:59 +0530

    AppLand

---
+++ MiniumApps/launcher3/src/main/java/co/siempo/phone/activities/DashboardActivity.java
@@ -0,0 +1,11 @@
+package co.siempo.phone.activities;
+
+import org.androidannotations.annotations.EActivity;
+
+import co.siempo.phone.R;
+
+@EActivity(R.layout.activity_dashboard)
+public class DashboardActivity extends BaseActivity {
+
+
+}
