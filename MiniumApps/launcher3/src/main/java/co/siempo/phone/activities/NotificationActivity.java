17b1faff307e98caa23e59eb4e88f22e19bc8159
Hardik Kamothi <hardik.kamothi@volansystech.com>
2018-03-06 16:04:13 +0530

    SSA-1294 Minor UI issues in Notification Settings          - Solve issue

--- MiniumApps/launcher3/src/main/java/co/siempo/phone/activities/NotificationActivity.java
+++ MiniumApps/launcher3/src/main/java/co/siempo/phone/activities/NotificationActivity.java
@@ -97,7 +97,7 @@ public class NotificationActivity extends CoreActivity {
         // Initialize components
         toolbar = findViewById(R.id.toolbar);
         toolbar.setNavigationIcon(R.drawable.ic_arrow_back_blue_24dp);
-        toolbar.setTitle(R.string.allow_specific_apps);
+        toolbar.setTitle(R.string.select_apps_title);
         toolbar.setTitleTextColor(ContextCompat.getColor(this, R.color.colorAccent));
         toolbar.setNavigationOnClickListener(new View.OnClickListener() {
             @Override
