1c3036f4f9c2357137f482b52bafdd8ef05ddb35
Roma Chaudhary <roma.chaudhary@volansystech.com>
2018-04-13 16:41:50 +0530

    SSA-1375: Typing @ was laggy

--- MiniumApps/launcher3/src/main/java/co/siempo/phone/customviews/SearchLayout.java
+++ MiniumApps/launcher3/src/main/java/co/siempo/phone/customviews/SearchLayout.java
@@ -104,13 +104,13 @@ public class SearchLayout extends CardView {
 
             @Override
             public void onTextChanged(CharSequence s, int start, int before, int count) {
-                handleAfterTextChanged(s.toString());
-                DashboardActivity.isTextLenghGreater = s.toString();
+
             }
 
             @Override
             public void afterTextChanged(Editable s) {
-
+                handleAfterTextChanged(s.toString());
+                DashboardActivity.isTextLenghGreater = s.toString();
             }
         });
     }
