5fb010a63e1d231a3221b7c84a9eb0b8e38819e9
Roma Chaudhary <roma.chaudhary@volansystech.com>
2018-04-25 17:05:00 +0530

    Fixed: SSA-1628, SSA-1626, SSA-1627

--- MiniumApps/launcher3/src/main/java/co/siempo/phone/activities/EmailRequestActivity.java
+++ MiniumApps/launcher3/src/main/java/co/siempo/phone/activities/EmailRequestActivity.java
@@ -235,13 +235,11 @@ public class EmailRequestActivity extends CoreActivity implements View.OnClickLi
             } catch (Exception e) {
                 e.printStackTrace();
             }
-            relPrivacyEmail.setVisibility(View.GONE);
-            viewFlipperEmail.setInAnimation(this, R.anim
-                    .in_from_right_email);
-            viewFlipperEmail.setOutAnimation(this, R.anim
-                    .out_to_left_email);
-            UIUtils.hideSoftKeyboard(this, getWindow().getDecorView().getWindowToken());
-            viewFlipperEmail.showNext();
+            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+                flipView();
+            } else {
+                finish();
+            }
         } else {
             autoCompleteTextViewEmail.requestFocus();
             txtErrorMessage.setVisibility(View.VISIBLE);
