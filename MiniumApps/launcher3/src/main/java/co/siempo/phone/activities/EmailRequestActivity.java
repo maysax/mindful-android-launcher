b9d3f911f9eeeeb4ca4ef857f9c6478076bd4460
Roma Chaudhary <roma.chaudhary@volansystech.com>
2018-05-14 15:23:03 +0530

    - quick fix

--- MiniumApps/launcher3/src/main/java/co/siempo/phone/activities/EmailRequestActivity.java
+++ MiniumApps/launcher3/src/main/java/co/siempo/phone/activities/EmailRequestActivity.java
@@ -61,9 +61,9 @@ public class EmailRequestActivity extends CoreActivity implements View.OnClickLi
         permissionUtil = new PermissionUtil(this);
         Window window = getWindow();
         window.addFlags(WindowManager.LayoutParams.FLAG_DRAWS_SYSTEM_BAR_BACKGROUNDS);
-        window.setStatusBarColor(getResources().getColor(R.color.white));
         View decor = window.getDecorView();
         if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+            window.setStatusBarColor(getResources().getColor(R.color.white));
             decor.setSystemUiVisibility(View.SYSTEM_UI_FLAG_LIGHT_STATUS_BAR);
         }
         initView();
