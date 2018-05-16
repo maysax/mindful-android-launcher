44277c0547d0c86b9d952f56632043a5ee6700f0
rajeshj <rajesh.jadi@volansystech.com>
2018-05-16 15:18:25 +0530

    SSA-1721 Fullscreen Background Image only for Home Screen

--- MiniumApps/launcher3/src/main/java/co/siempo/phone/activities/DashboardActivity.java
+++ MiniumApps/launcher3/src/main/java/co/siempo/phone/activities/DashboardActivity.java
@@ -37,6 +37,7 @@ import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 import com.google.firebase.database.ValueEventListener;
+import com.readystatesoftware.systembartint.SystemBarTintManager;
 
 import java.util.Objects;
 import java.util.concurrent.RejectedExecutionException;
@@ -189,6 +190,11 @@ public class DashboardActivity extends CoreActivity {
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         boolean read = PrefSiempo.getInstance(this).read(PrefSiempo.IS_DARK_THEME, false);
         setTheme(read ? R.style.SiempoAppThemeDark : R.style.SiempoAppTheme);
+
+        Window w = getWindow(); // in Activity's onCreate() for instance
+        w.setFlags(WindowManager.LayoutParams.FLAG_TRANSLUCENT_NAVIGATION, WindowManager.LayoutParams.FLAG_TRANSLUCENT_NAVIGATION);
+        w.setFlags(WindowManager.LayoutParams.FLAG_TRANSLUCENT_STATUS, WindowManager.LayoutParams.FLAG_TRANSLUCENT_STATUS);
+
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_dashboard);
         linMain = findViewById(R.id.linMain);
@@ -201,19 +207,22 @@ public class DashboardActivity extends CoreActivity {
         if (startTime == 0) {
             startTime = System.currentTimeMillis();
         }
-        mWindow = getWindow();
-        // clear FLAG_TRANSLUCENT_STATUS flag:
-        if (null != mWindow) {
-            mWindow.clearFlags(WindowManager.LayoutParams.FLAG_TRANSLUCENT_STATUS);
-
-            // add FLAG_DRAWS_SYSTEM_BAR_BACKGROUNDS flag to the window
-            mWindow.addFlags(WindowManager.LayoutParams.FLAG_DRAWS_SYSTEM_BAR_BACKGROUNDS);
-            defaultStatusBarColor = mWindow.getStatusBarColor();
-        }
-        Log.d("Test", "P2");
         permissionUtil = new PermissionUtil(this);
         overlayDialog = new Dialog(this, 0);
         showOverlayOfDefaultLauncher();
+        View decor = w.getDecorView();
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M && !read) {
+            decor.setSystemUiVisibility(View.SYSTEM_UI_FLAG_LIGHT_STATUS_BAR);
+        }
+    }
+
+    public int getStatusBarHeight() {
+        int result = 0;
+        int resourceId = getResources().getIdentifier("status_bar_height", "dimen", "android");
+        if (resourceId > 0) {
+            result = getResources().getDimensionPixelSize(resourceId);
+        }
+        return result;
     }
 
     private void changeLayoutBackground() {
@@ -278,6 +287,8 @@ public class DashboardActivity extends CoreActivity {
     public void loadViews() {
 
         mPager = findViewById(R.id.pager);
+        linMain = findViewById(R.id.linMain);
+        mPager.setPadding(0, getStatusBarHeight(), 0, 0);
         mPagerAdapter = new DashboardPagerAdapter(getFragmentManager());
         loadPane();
         mPager.setAdapter(mPagerAdapter);
@@ -336,6 +347,9 @@ public class DashboardActivity extends CoreActivity {
                     Log.d("Firebase", "Intention Start");
                     startTime = System.currentTimeMillis();
                 }
+
+                changeColorOfStatusbar(i);
+
                 currentIndexDashboard = i;
             }
 
@@ -355,6 +369,26 @@ public class DashboardActivity extends CoreActivity {
         }
 
 
+    }
+
+    void changeColorOfStatusbar(int pos) {
+        SystemBarTintManager tintManager = new SystemBarTintManager(this);
+// set the transparent color of the status bar, 20% darker
+//        if (pos == 0) {
+//// enable status bar tint
+//            tintManager.setStatusBarTintEnabled(true);
+//// enable navigation bar tint
+//            tintManager.setNavigationBarTintEnabled(true);
+//
+//            tintManager.setStatusBarTintColor(R.color.black);
+//        } else {
+            // enable status bar tint
+            tintManager.setStatusBarTintEnabled(true);
+// enable navigation bar tint
+            tintManager.setNavigationBarTintEnabled(true);
+
+            tintManager.setStatusBarTintColor(R.color.transparent);
+//        }
     }
 
     private void loadPane() {
@@ -618,7 +652,6 @@ public class DashboardActivity extends CoreActivity {
             startActivity(startMain);
 
         }
-
     }
 
 
