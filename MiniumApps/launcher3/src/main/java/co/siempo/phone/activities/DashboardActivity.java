979217c7cc47e5bdd770732233faabb68095dae2
rajeshj <rajesh.jadi@volansystech.com>
2018-05-17 10:51:07 +0530

    Navigation bar issue fixing

--- MiniumApps/launcher3/src/main/java/co/siempo/phone/activities/DashboardActivity.java
+++ MiniumApps/launcher3/src/main/java/co/siempo/phone/activities/DashboardActivity.java
@@ -8,6 +8,7 @@ import android.content.Context;
 import android.content.DialogInterface;
 import android.content.Intent;
 import android.content.pm.ActivityInfo;
+import android.content.res.Resources;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
 import android.graphics.drawable.BitmapDrawable;
@@ -25,6 +26,7 @@ import android.text.TextUtils;
 import android.util.Log;
 import android.view.Gravity;
 import android.view.View;
+import android.view.ViewConfiguration;
 import android.view.Window;
 import android.view.WindowManager;
 import android.widget.Button;
@@ -192,7 +194,10 @@ public class DashboardActivity extends CoreActivity {
 
         Window w = getWindow(); // in Activity's onCreate() for instance
         w.setFlags(WindowManager.LayoutParams.FLAG_TRANSLUCENT_NAVIGATION, WindowManager.LayoutParams.FLAG_TRANSLUCENT_NAVIGATION);
-        w.setFlags(WindowManager.LayoutParams.FLAG_TRANSLUCENT_STATUS, WindowManager.LayoutParams.FLAG_TRANSLUCENT_STATUS);
+//        w.setFlags(WindowManager.LayoutParams.FLAG_TRANSLUCENT_STATUS, WindowManager.LayoutParams.FLAG_TRANSLUCENT_STATUS);
+//        w.setFlags(WindowManager.LayoutParams.FLAG_LAYOUT_NO_LIMITS, WindowManager.LayoutParams.FLAG_LAYOUT_NO_LIMITS);
+
+
 
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_dashboard);
@@ -218,6 +223,15 @@ public class DashboardActivity extends CoreActivity {
     public int getStatusBarHeight() {
         int result = 0;
         int resourceId = getResources().getIdentifier("status_bar_height", "dimen", "android");
+        if (resourceId > 0) {
+            result = getResources().getDimensionPixelSize(resourceId);
+        }
+        return result;
+    }
+
+    public int getNavigationBarHeight() {
+        int result = 0;
+        int resourceId = getResources().getIdentifier("navigation_bar_height", "dimen", "android");
         if (resourceId > 0) {
             result = getResources().getDimensionPixelSize(resourceId);
         }
@@ -281,13 +295,29 @@ public class DashboardActivity extends CoreActivity {
         // this overlay of default launcher if siempo is not set as default
         // launcher
         showOverlayOfDefaultLauncher();
+        if(read){
+            getWindow().setNavigationBarColor(getResources().getColor(R.color.transparent));
+        }else{
+            getWindow().setNavigationBarColor(getResources().getColor(R.color.black));
+        }
+    }
+
+    public boolean hasNavBar(Resources resources) {
+        int id = resources.getIdentifier("config_showNavigationBar", "bool", "android");
+        return id > 0 && resources.getBoolean(id);
     }
 
     public void loadViews() {
 
         mPager = findViewById(R.id.pager);
         linMain = findViewById(R.id.linMain);
-        mPager.setPadding(0, getStatusBarHeight(), 0, 0);
+        boolean hasMenuKey = ViewConfiguration.get(this).hasPermanentMenuKey();
+        if (hasNavBar(getResources())) {
+            mPager.setPadding(0, getStatusBarHeight(), 0, getNavigationBarHeight());
+        } else {
+            mPager.setPadding(0, getStatusBarHeight(), 0, 0);
+
+        }
         mPagerAdapter = new DashboardPagerAdapter(getFragmentManager());
         loadPane();
         mPager.setAdapter(mPagerAdapter);
