8058e1d412215718ebb8d89221b2e14edc499480
Roma Chaudhary <roma.chaudhary@volansystech.com>
2018-05-17 18:28:44 +0530

    - increasing overlay till 50% of height

--- MiniumApps/launcher3/src/main/java/co/siempo/phone/service/OverlayService.java
+++ MiniumApps/launcher3/src/main/java/co/siempo/phone/service/OverlayService.java
@@ -4,9 +4,13 @@ import android.app.Service;
 import android.content.Context;
 import android.content.Intent;
 import android.graphics.PixelFormat;
+import android.graphics.Point;
 import android.os.Build;
+import android.os.Handler;
 import android.os.IBinder;
 import android.support.annotation.Nullable;
+import android.view.Display;
+import android.view.Gravity;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -22,6 +26,9 @@ public class OverlayService extends Service {
 
     private WindowManager wm;
     private View androidHead;
+    private Handler handler;
+    private int delay;
+    private int heightWindow;
 
     @Nullable
     @Override
@@ -39,7 +46,8 @@ public class OverlayService extends Service {
             wm = (WindowManager) getSystemService(WINDOW_SERVICE);
             final WindowManager.LayoutParams params = new WindowManager.LayoutParams();
             params.width = ViewGroup.LayoutParams.MATCH_PARENT;
-            params.height = ViewGroup.LayoutParams.MATCH_PARENT;
+            params.height = 200;
+            params.gravity = Gravity.BOTTOM;
 
             if (Build.VERSION.SDK_INT < Build.VERSION_CODES.O) {
                 params.type = WindowManager.LayoutParams.TYPE_SYSTEM_ALERT;
@@ -55,6 +63,32 @@ public class OverlayService extends Service {
             if (null != wm) {
                 wm.addView(androidHead, params);
             }
+            handler = new Handler();
+            //milliseconds
+            delay = 1000;
+            Display display = wm.getDefaultDisplay();
+            Point size = new Point();
+            display.getSize(size);
+
+            heightWindow = size.y / 2;
+            handler.postDelayed(new Runnable() {
+                public void run() {
+                    try {
+                        if (params.height <= heightWindow) {
+                            //Increase height of overlay
+                            params.height = params.height + 100;
+                            androidHead.setLayoutParams(new ViewGroup.LayoutParams(params));
+                            wm.updateViewLayout(androidHead, params);
+                            handler.postDelayed(this, delay);
+                        } else {
+                            handler.removeCallbacksAndMessages(null);
+                        }
+                    } catch (Exception e) {
+                        e.printStackTrace();
+                    }
+                }
+            }, delay);
+
         } catch (Exception e) {
             e.printStackTrace();
         }
@@ -65,6 +99,7 @@ public class OverlayService extends Service {
     @Override
     public void onDestroy() {
         super.onDestroy();
+        handler.removeCallbacksAndMessages(null);
         removeView();
     }
 
