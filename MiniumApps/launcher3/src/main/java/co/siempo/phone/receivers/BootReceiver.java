71a1b16ad7b31ea264376bc9126ccba9d0de7cf3
Roma Chaudhary <roma.chaudhary@volansystech.com>
2018-06-14 14:35:53 +0530

    SSA-1828: Restart issue fixed

--- MiniumApps/launcher3/src/main/java/co/siempo/phone/receivers/BootReceiver.java
+++ MiniumApps/launcher3/src/main/java/co/siempo/phone/receivers/BootReceiver.java
@@ -24,9 +24,9 @@ public class BootReceiver extends BroadcastReceiver {
         Log.d(TAG, "Boot complete");
         try {
             PrefSiempo.getInstance(context).write(PrefSiempo.CALL_RUNNING, false);
-            Intent myService = new Intent(context, StatusBarService.class);
-            context.startService(myService);
-            EventBus.getDefault().post(new ReduceOverUsageEvent(false));
+//            Intent myService = new Intent(context, StatusBarService.class);
+//            context.startService(myService);
+//            EventBus.getDefault().post(new ReduceOverUsageEvent(false));
         } catch (Exception e) {
             e.printStackTrace();
         }
